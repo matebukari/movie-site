e07a6d787bb96f6868b5e9d2c9d40a7eb16e1410
Bukari Máté <matebukari@gmail.com>
2025-10-28 22:11:52 +0100

    Set up server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+// server.js
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+// Routes
+
+app.get("/", (req, res) => {
+  res.send("Catalogue API is running 🚀");
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
